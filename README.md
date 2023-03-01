7f18e169ffd376ab40632a0890718e8689ad50d3
Kaitlyn <k6chan@ucsd.edu>
2023-03-01 03:25:44 -0800

    add new features, stemming etc, test doc2vec

--- README.md
+++ README.md
@@ -8,18 +8,6 @@ A reverse dictionary for Pokemon as a Python Flask web app.
 
 *With approval from my TA, a `run.py` script is not necessary for this website project.*
 
-### Without Docker
-
-First, clone the repository to your device with `git clone`.
-
-Install the Python libraries in `requirements.txt` with `pip install --no-cache-dir -r requirements.txt`.
-
-Change directory into `src/models` and use the following command to run the dev server:
-
-`python -m flask --app application run`
-
-Access the server in your browser, usually `http://127.0.0.1:5000/`.
-
 ### With Docker (automatically installs libraries and starts the dev server)
 
 **The Flask server hosting is not compatible with Docker on DSMLP. This Docker image can only be run locally, not on DSMLP.**
@@ -34,7 +22,18 @@ Run the Docker image using `docker run -it --rm -p 5000:5000 k6chan/reverse-dict
 
 The Flask server should automatically start up. Access the server in your browser, usually `http://127.0.0.1:5000/`.
 
-If you receive an error in the LocalProxy stating `__init__() got an unexpected keyword argument 'unbound_message'`, downgrade to an older version of Flask 2.2.x: `pip install flask==2.2.0`
+### Without Docker
+
+First, clone the repository to your device with `git clone`.
+
+Install the Python libraries in `requirements.txt` with `pip install --no-cache-dir -r requirements.txt`.
+
+Change directory into `src/models` and use the following command to run the dev server:
+
+`python -m flask --app application run`
+
+Access the server in your browser, usually `http://127.0.0.1:5000/`.
 
 ## Contribution Statement
+
 Solo project by Kaitlyn Chan.
--- src/models/application.py
+++ src/models/application.py
@@ -3,20 +3,48 @@ import pandas as pd
 import sklearn.feature_extraction as fe
 from sklearn.metrics.pairwise import cosine_similarity
 
+import spacy
+
+import gensim.downloader
+from gensim.models import Word2Vec
+from gensim.models import KeyedVectors
+from gensim.models.doc2vec import Doc2Vec, TaggedDocument
+from scipy import spatial
+
 
 app = Flask(__name__)
 
 @app.route("/", methods=["GET","POST"])
 def query():
     if request.method == "POST":
+        functions = {
+            "ir_tf_idf": ir_tf_idf,
+            "ir_tf_idf_word2vec": ir_tf_idf_word2vec
+        }
+        
         user_query = request.form.get("query")
-        results_ir_tf_idf = ir_tf_idf(user_query)
-        return render_template("output.html",pokemon_1=results_ir_tf_idf[0],pokemon_2=results_ir_tf_idf[1],
-        pokemon_3=results_ir_tf_idf[2],pokemon_4=results_ir_tf_idf[3],pokemon_5=results_ir_tf_idf[4])
+        model_type = functions[request.form.get("model_type")]
+        results = model_type(clean_query(user_query))
+        return render_template("output.html",pokemon_1=results[0],pokemon_2=results[1],
+        pokemon_3=results[2],pokemon_4=results[3],pokemon_5=results[4])
     return render_template("input.html")
 
 
 df = pd.read_csv("data/bulbapedia_smogon_data.csv")
+
+
+nlp = spacy.load("data/vocabulary/en_core_web_sm-3.5.0")
+def clean_query(raw_query):
+    #lowercase, stem, remove stopwords
+    lowercase = raw_query.lower()
+    document = nlp(lowercase)
+    tokens = []
+    for token in document:
+        if token.text not in spacy.lang.en.stop_words.STOP_WORDS:
+            tokens.append(token.lemma_)
+    cleaned_query = " ".join(tokens)
+    return cleaned_query
+
 
 model = fe.text.TfidfVectorizer(input="content")
 vector = model.fit_transform(df["description"])
@@ -28,3 +56,13 @@ def ir_tf_idf(query):
     for i in range(5):
         top_5.append((df_sims.iloc[i]["name"],df_sims.iloc[i]["thumbnail"]))
     return top_5
+
+
+w2v_model =  KeyedVectors.load('data/vocabulary/glove-wiki-gigaword-50_vectors.bin')
+def ir_tf_idf_word2vec(query):
+    sims = []
+    large_query = query
+    for token in query.split():
+        additional_words = list(map(lambda item: item[0], w2v_model.most_similar(token)))
+        large_query = large_query + " " + " ".join(additional_words)
+    return ir_tf_idf(large_query)
